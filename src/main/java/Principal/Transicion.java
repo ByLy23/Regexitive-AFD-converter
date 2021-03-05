de61fcaef24b32dcf10cd24ad56c28434ecca5d6
Byron <3018080310101@ingenieria.usac.edu.gt>
2021-03-05 16:39:53 -0600

    Media tabla de transiciones.

--- src/main/java/Principal/Transicion.java
+++ src/main/java/Principal/Transicion.java
@@ -14,8 +14,26 @@ import java.util.ArrayList;
 public class Transicion {
     private String nombreEstado;
     private boolean visitado;
-    private ArrayList<Integer> listatrancisiones;
+    private ArrayList<Integer> listaEstados;
     private boolean aceptacion;
+    private ArrayList<Transicion> listaTrancisiones;
+
+    public ArrayList<Integer> getListaEstados() {
+        return listaEstados;
+    }
+
+    public void setListaEstados(ArrayList<Integer> listaEstados) {
+        this.listaEstados = listaEstados;
+    }
+
+    public ArrayList<Transicion> getListaTrancisiones() {
+        return listaTrancisiones;
+    }
+
+    public void setListaTrancisiones(ArrayList<Transicion> listaTrancisiones) {
+        this.listaTrancisiones = listaTrancisiones;
+    }
+
 
     public String getNombreEstado() {
         return nombreEstado;
@@ -34,11 +52,11 @@ public class Transicion {
     }
 
     public ArrayList<Integer> getListatrancisiones() {
-        return listatrancisiones;
+        return listaEstados;
     }
 
     public void setListatrancisiones(ArrayList<Integer> listatrancisiones) {
-        this.listatrancisiones = listatrancisiones;
+        this.listaEstados = listatrancisiones;
     }
 
     public boolean isAceptacion() {
@@ -50,10 +68,11 @@ public class Transicion {
     }
 
     public Transicion() {
-        this.listatrancisiones= new ArrayList<>();
+        this.listaEstados= new ArrayList<>();
     }
-    public Transicion generarTabla(int aceptacion, ArrayList<Integer> raiz, ArrayList<Siguiente> listaSigs){
-        Transicion aux=null;
-        return aux;
+  public Transicion(String nombre, ArrayList<Integer> estados){
+      this.nombreEstado=nombre;
+      this.listaEstados= estados;
+      this.listaTrancisiones=new ArrayList<>();
     }
 }
